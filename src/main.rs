0770dcac86273b0c8c924afe62dfa15f2854bcbe
KLohger <fanimdude32@gmail.com>
2023-08-08 15:41:18 +0200

    pain

--- src/main.rs
+++ src/main.rs
@@ -195,5 +195,5 @@ fn the_source<'a>(ws: ws::WebSocket, mut shutdown: Shutdown, vessels: &'a State<
 
 #[rocket::launch]
 async fn launch() -> _ {
-  rocket::custom(rocket::Config { port: 80, ..Default::default() }).manage(Vessels::default()).mount("/", routes![index, css, source_connector, status_icon, the_source])
+  rocket::custom(rocket::Config { port: 3000, ..Default::default() }).manage(Vessels::default()).mount("/", routes![index, css, source_connector, status_icon, the_source])
 }
